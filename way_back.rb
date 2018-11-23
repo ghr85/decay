2524a1f9f32b086a51597817eeade5dfd364521f
ghr85 <ghrenfrew@gmail.com>
2018-11-23 06:32:33 +0000

    initial upload

---
+++ way_back.rb
@@ -0,0 +1,14 @@
+puts "What date is it? (dd/mm/yyyy)"
+today_date = gets.chomp
+sleep 2
+def way_back
+  puts "Welcome to your Way Back coding challenge"
+end
+way_back
+puts "Named so because..."
+sleep 2
+puts "You set this challenge on..."
+sleep 2
+puts "todays date is #{today_date}"
+puts "this is what you have to show for x days work."
+puts " You have y days to go."
