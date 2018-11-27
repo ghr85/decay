fec84ffe84e5bfca572da3f38304b514f5bae5d8
ghr85 <ghrenfrew@gmail.com>
2018-11-27 06:16:27 +0000

    Required DateTime function, assigned new variables for beggining and end dates.

--- way_back.rb
+++ way_back.rb
@@ -1,14 +1,22 @@
-puts "What date is it? (dd/mm/yyyy)"
-today_date = gets.chomp
-sleep 2
+
+require 'date'
+
+today_date = Time.now.strftime("%Y-%m-%d")
+begin_date = Date.parse("2018-09-14")
+course_date = Date.parse("2019-01-14")
+job_date = Date.parse("2019-05-03")
+
+
+
+sleep 1
 def way_back
   puts "Welcome to your Way Back coding challenge"
 end
 way_back
 puts "Named so because..."
-sleep 2
-puts "You set this challenge on..."
-sleep 2
-puts "todays date is #{today_date}"
-puts "this is what you have to show for x days work."
+sleep 1
+puts "You set this challenge on the way back from your CodeClan
+#interview on #{begin_date}"
+sleep 1
+puts "Today's date is #{today_date}"
 puts " You have y days to go."
