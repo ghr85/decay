94925993f7b67e4ba1ffd24c21b077b6a6236b7e
ghr85 <ghrenfrew@gmail.com>
2018-11-27 07:05:50 +0000

    It works! It bloody works! Finally! Parsing the date using Date.parse is key to this. Added this function rather than a declaration of the current time. Assigned user input to the date parse - now have a day that arithmetic can be performed with. Still needs further polish.

--- way_back.rb
+++ way_back.rb
@@ -5,7 +5,7 @@ today_date = Time.now.strftime("%Y-%m-%d")
 begin_date = Date.parse("2018-09-14")
 course_date = Date.parse("2019-01-14")
 job_date = Date.parse("2019-05-03")
-
+#days_remaining = Date.parse(course_date - today_date)
 
 
 sleep 1
@@ -13,10 +13,20 @@ def way_back
   puts "Welcome to your Way Back coding challenge"
 end
 way_back
+sleep 1
 puts "Named so because..."
 sleep 1
 puts "You set this challenge on the way back from your CodeClan
-#interview on #{begin_date}"
+interview on #{begin_date}"
 sleep 1
 puts "Today's date is #{today_date}"
-puts " You have y days to go."
+sleep 1
+puts "The course begins on...#{begin_date}"
+sleep 1
+puts "The course ends on #{job_date}"
+sleep 1
+#puts " You have #{days_remaining} days to prep."
+sleep 1
+puts " You have y days to prep."
+sleep 1
+puts " Get tore intae it!"
