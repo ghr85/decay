cff92b9cdd6e039c418d9d715af89a00786d35a0
ghr85 <ghrenfrew@gmail.com>
2018-11-27 18:24:11 +0000

    Updated the variables for Days remaining to remove remainder by using to_i function.

--- way_back.rb
+++ way_back.rb
@@ -1,32 +1,38 @@
 
 require 'date'
 
-today_date = Time.now.strftime("%Y-%m-%d")
-begin_date = Date.parse("2018-09-14")
-course_date = Date.parse("2019-01-14")
-job_date = Date.parse("2019-05-03")
-#days_remaining = Date.parse(course_date - today_date)
-
-
+puts "Hello there!"
 sleep 1
+puts "What is todays date?"
+date_input = gets.chomp
+
+today_date = Date.parse("#{date_input}")
+begin_date = Date.parse("14th September 2018")
+course_date = Date.parse("14th January 2019")
+job_date = Date.parse("3rd May 2019")
+days_remaining = (course_date - today_date).to_i
+d_day = (job_date - today_date).to_i
+
+
 def way_back
   puts "Welcome to your Way Back coding challenge"
 end
+sleep 2
 way_back
-sleep 1
+sleep 1.5
 puts "Named so because..."
-sleep 1
+sleep 1.5
 puts "You set this challenge on the way back from your CodeClan
 interview on #{begin_date}"
-sleep 1
+sleep 1.5
 puts "Today's date is #{today_date}"
-sleep 1
+sleep 1.5
 puts "The course begins on...#{begin_date}"
-sleep 1
+sleep 1.5
 puts "The course ends on #{job_date}"
-sleep 1
-#puts " You have #{days_remaining} days to prep."
-sleep 1
-puts " You have y days to prep."
-sleep 1
-puts " Get tore intae it!"
+sleep 1.5
+puts " You have #{days_remaining} days to prep."
+sleep 1.5
+puts "You have #{d_day} days until the wolves are at the door"
+sleep 1.5
+puts "You are the wolf. Get tore intae it!"
